74eec50a1a5de80e0b2875c0fa4315619ef3ac2f
Otho Mantegazza <otho.mantegazza@gmail.com>
2023-01-28 18:41:56 +0000

    translate palette from Fabio Crameri into json object

---
+++ js/translate-palettes.js
@@ -0,0 +1,41 @@
+const convert = require('xml-js');
+const fs = require('fs');
+
+convertScicoPalette('data/tokyo_PARAVIEW.xml', 'data/tokyo.json')
+
+function convertScicoPalette(path_in, path_out) {
+  fs.readFile(path_in, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    const pal = convert.xml2json(data, { compact: true, spaces: 4 })
+    // console.log(JSON.parse(pal))
+    const pal_js = JSON.parse(pal)
+    const points = pal_js.ColorMaps.ColorMap.Point
+    const pointsHex = points.map(point => toHexColor(point))
+    fs.writeFile(
+      path_out,
+      JSON.stringify(pointsHex),
+      err => {
+        if (err) {
+          console.error(err);
+        }
+        // file written successfully
+      })
+  })
+}
+
+function toHexColor(point) {
+  point = point._attributes
+  const r = numberstringToHex(point.r)
+  const g = numberstringToHex(point.g)
+  const b = numberstringToHex(point.b)
+  return (`#${r}${g}${b}`)
+}
+
+function numberstringToHex(str) {
+  const intensity = Math.trunc(parseFloat(str) * 255)
+  const hex = intensity.toString(16)
+  return (hex)
+}
