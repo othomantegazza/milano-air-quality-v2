30a51fe83b99e45ea21b2a9837e15a5c1789963d
Otho Mantegazza <otho.mantegazza@gmail.com>
2023-02-20 00:42:18 +0100

    table results

---
+++ js/table.js
@@ -0,0 +1,45 @@
+function tableDays(data) {
+
+    d = d3.create('div')
+
+    d.append('table')
+        .attr('id', 'daystable')
+        .html(`<table>
+                    <th>Pollutant</th>
+                    <th>Pollutant Symbol</th>
+                    <th>Days Measured</th>
+                    <th>Days Above Limits</th>
+                    <th>Frequency</th>
+                    ${makeRow(data, 'Cylcic Aromatics', 'C6H6')}
+                    ${makeRow(data, 'Carbon Monoxide', 'CO')}
+                    ${makeRow(data, 'Nitrogen Dioxide', 'NO2')}
+                    ${makeRow(data, 'Ozone', 'O3')}
+                    ${makeRow(data, 'PM10', 'PM10')}
+                    ${makeRow(data, 'PM2.5', 'PM25')}
+                    ${makeRow(data, 'Sulphur Dioxide', 'SO2')}
+                </table>`)
+
+    console.log(makeRow(data, 'Cylcic Aromatics', 'C6H6'))
+
+    return d.node()
+}
+
+function makeRow(data, pollName, pollutant) {
+    let daysMeasured = data.filter((i) => {
+        return (i.inquinante === pollutant)
+    })
+    let daysAboveLimit = daysMeasured.filter((i) => {
+        return (i.valore > i.eu_limits)
+    })
+    console.log({daysMeasured, daysAboveLimit})
+    console.log(daysMeasured.length)
+    let out
+    out = `<tr>
+                <td>${pollName}</td>
+                <td>${pollutant}</td>
+                <td>${daysMeasured.length}</td>
+                <td>${daysAboveLimit.length}</td>
+                <td>${d3.format('.0%')(daysAboveLimit.length/daysMeasured.length)}</td>
+            </tr>`
+    return out
+}
