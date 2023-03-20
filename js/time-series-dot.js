4c729df2c95f72a9c467c6b182b585f868eb49d6
Otho Mantegazza <otho.mantegazza@gmail.com>
2023-03-20 15:20:14 +0100

    fix month number in scatterplot

--- js/time-series-dot.js
+++ js/time-series-dot.js
@@ -303,7 +303,7 @@ function Scatterplot(data, {
 
       function dateForLabel(msec) {
             const formatted = dayjs(msec)
-            return(`${formatted.$D}-${formatted.$M}-${formatted.$y}`)
+            return(`${formatted.$D}-${formatted.$M + 1}-${formatted.$y}`)
       }
 
       return svg.node();
